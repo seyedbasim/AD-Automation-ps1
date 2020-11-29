d5432c216d293b344c63d2e3a3c5a0f81c78f56b
Seyed Basim <Seyedbasim95@outlook.com>
2020-11-29 14:15:02 +0530

    Intial Scripts Upload

---
+++ AdGrouplistinCSV.ps1
@@ -0,0 +1,19 @@
+﻿$Users = Get-ADUser -Filter * -SearchBase "OU=LKA,OU=MidasSafety,DC=midassafety,DC=com"
+Add-Content -Path C:\GroupDetails.csv  -Value '"Name","Groups"'
+foreach($user in $users){
+  $Groups = Get-ADPrincipalGroupMembership -Identity $user.SID
+      $hash = @{
+                 "Name" = $user.Name
+                 "Groups" = "-"
+                 }
+      $newRow = New-Object PsObject -Property $hash
+      Export-Csv C:\GroupDetails.csv -inputobject $newrow -append -Force
+  foreach($Group in $Groups){
+      $hash = @{
+             "Name" = ""
+             "Groups" = $Group.name
+             }
+      $newRow = New-Object PsObject -Property $hash
+      Export-Csv C:\GroupDetails.csv -inputobject $newrow -append -Force
+  }
+}
---
+++ Remove ADobjects.ps1
@@ -0,0 +1,5 @@
+﻿$computers = Import-Csv "C:\Users\Basim\Desktop\removeAD\list.csv"
+$computers | ForEach-Object{
+    $computer = $_
+    Get-ADComputer -Identity $computer.PCName | Remove-ADObject -Confirm:$false -Recursive
+}
